de30f7253434b3fe0db38b8e490f50dff9ccc0f1
daniel_2k <38509254+daniel-2k@users.noreply.github.com>
2023-07-16 15:45:51 +0200

    js-controller 5

--- test/integration.js
+++ test/integration.js
@@ -3,5 +3,5 @@ const { tests } = require('@iobroker/testing');
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 tests.integration(path.join(__dirname, '..'),{
-  controllerVersion: "4.0.23"
+  controllerVersion: 5.0.8"
 });
